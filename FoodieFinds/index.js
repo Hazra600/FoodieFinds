a677d54b9420a305308e05b8835b422705fd1da6
Hazra600 <git@stackblitz.com>
2024-12-18 21:55:37 +0530

    Initial commit

---
+++ FoodieFinds/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose();
+const { open } = require('sqlite');
+// const { resolve } = require('path');
+
+const app = express();
+const port = 3000;
+
+// app.use(express.static('static'));
+app.use(cors());
+app.use(express.json());
+
+let db;
+
+(async () => {
+  db = await open({
+    filename: './FoodieFinds/database.sqlite',
+    driver: sqlite3.database,
+  });
+})();
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
